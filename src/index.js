7c3ebe06cbb45e6f1de3a9bbfbb2a17111432295
Bushra Khugyani <bushrakhugyani@gmail.com>
2024-02-22 19:37:43 -0800

    injected Function to get the weather forecast for a given city through response from api

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// Function to update the weather information on the page
 function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
@@ -16,7 +17,11 @@ function refreshWeather(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}mph`;
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon"/>`;
+  //now to get Forecast data for the city searched function is called bellow
+  getForecast(response.data.city);
 }
+
+// Function to format a date object into a readable date string
 function formatDate(date) {
   let days = [
     "Sunday",
@@ -37,12 +42,14 @@ function formatDate(date) {
   return `${day} ${hours}:${minutes}`;
 }
 
+// Function to search for weather information for a given city
 function searchCity(city) {
   let apiKey = "98f12063t8f0b4be43fb6oa12441998c";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(refreshWeather);
 }
 
+// Event handler for the search form submission
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
@@ -51,7 +58,15 @@ function handleSearchSubmit(event) {
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-function displayForecast() {
+// Function to get the weather forecast for a given city
+function getForecast(city) {
+  let apiKey = "98f12063t8f0b4be43fb6oa12441998c";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=imperial`;
+  axios(apiUrl).then(displayForecast); // we can write axios(apiUrl) or axios.get(apiUrl)its sameb
+}
+
+// Function to display the weather forecast on the page
+function displayForecast(response) {
   let days = ["Tue", "Wed", "The", "Fri", "Sat"];
   let forecastHtml = "";
   days.forEach(function (day) {
@@ -70,6 +85,7 @@ function displayForecast() {
   let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHtml;
 }
-displayForecast();
 
+// Initial setup: display forecast for default city and search for "sacramento"
+displayForecast();
 searchCity("sacramento");
